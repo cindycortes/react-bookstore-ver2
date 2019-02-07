8713622eba0baab22fa2e57d4ebca0c821e8b46d
Cindy Cortes <cindy.indins@gmail.com>
2019-02-07 16:24:47 -0700

    bookItemList to cartList

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ class App extends Component {
     books: []
   }
 
-  booksInCart = () => this.state.books.filter(book => book.inCart)
-
+  
   async componentDidMount() {
     const response = await fetch(`http://localhost:8082/api/books`)
     const books = await response.json(); 
@@ -21,37 +20,41 @@ class App extends Component {
   } catch (error) {
     console.log(error);
   }
-
+  
   addBookToCart = (id) => {
     axios.patch(`http://localhost:8082/api/books/cart/add/${id}`)
-      .then(res => {
-        let theOtherBooks = this.state.books.filter(book => book.id != id)
-        let orderedBooks = [...theOtherBooks, res.data].sort((a, b) => a.id > b.id)
-        this.setState({ books: orderedBooks })
-      })
+    .then(res => {
+      let theOtherBooks = this.state.books.filter(book => book.id != id)
+      let orderedBooks = [...theOtherBooks, res.data].sort((a, b) => a.id > b.id)
+      this.setState({ books: orderedBooks })
+    })
   }
-
+  
   removeBookFromCart = (id) => {
     axios.patch(`http://localhost:8082/api/books/cart/remove/${id}`)
-      .then(res => {
-        let theOtherBooks = this.state.books.filter(book => book.id != id)
-        let orderedBooks = [...theOtherBooks, res.data].sort((a, b) => a.id > b.id)
-        this.setState({ books: orderedBooks })
-      })
+    .then(res => {
+      let theOtherBooks = this.state.books.filter(book => book.id != id)
+      let orderedBooks = [...theOtherBooks, res.data].sort((a, b) => a.id > b.id)
+      this.setState({ books: orderedBooks })
+    })
   }
+  
 
+  cartList = () => this.state.books.filter(book => book.inCart)
 
   render() {
-    console.log('booksInCart', this.booksInCart())
+    console.log('cartList', this.cartList())
 
     return (
       <div className="App">
         <TopNav />
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
+
             <div className="col"><BookList books={this.state.books}
               addBookToCart={this.addBookToCart} /></div>
-            <div className="col"><Cart booksInCart={this.booksInCart()} removeBookFromCart={this.removeBookFromCart} /></div>
+
+            <div className="col"><Cart cartList={this.cartList()} removeBookFromCart={this.removeBookFromCart} /></div>
           </div>
         </div>
 
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import CartItem from './CartItem'
 
 const Cart = (props) => {
-    let listOfCartItems = props.booksInCart
+    let listOfCartItems = props.cartList
         .map(item => <CartItem key={item.id} item={item}
             removeBookFromCart={props.removeBookFromCart}/ >)
-    let theTotal = props.booksInCart.reduce((acc, item) => acc + item.price, 0)
+    let theTotal = props.cartList.reduce((acc, item) => acc + item.price, 0)
 
     return(
         <div> <h4>Cart Items: </h4> 
